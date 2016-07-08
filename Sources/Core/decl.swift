58ed6d9b9a66f135fe6665129a4b1060e3300fe2
Andreas Stührk <a.stuehrk@gini.net>
2016-07-09 00:23:01 +0200

    Simplify

--- Sources/Core/decl.swift
+++ Sources/Core/decl.swift
@@ -1,6 +1,1 @@
-public enum Either<LeftType, RightType> {
-           case Left(LeftType)
-           case Right(RightType)
-}
-
-public typealias Result<In, Out> = Either<(In, String), (In, Out)>
+public typealias Result<Out> = Optional<Out>
--- Sources/Crash/crash.swift
+++ Sources/Crash/crash.swift
@@ -1,7 +1,4 @@
 import Core
 
-private func createResult(
-    inputLeft: String.UnicodeScalarView,
-    result: String.UnicodeScalarView
-) -> Result<String.UnicodeScalarView, String.UnicodeScalarView> {
+private func createResult(result: String) -> Result<String> {
 }
